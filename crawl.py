7f4cc8d68ca06f136a2612096348bf8dabbc56ce
Alex Entwistle <alexentwistleseo@gmail.com>
2020-01-08 15:19:07 +0000

    Added explanation to crawl.py

--- crawl.py
+++ crawl.py
@@ -1,4 +1,6 @@
 # Crawl local council website to determine the day of rubbish collection.
+# Crawl URL. Retrieve response. Create output.html and write HTML response.
+
 import requests 
 
 url = "https://new.enfield.gov.uk/services/rubbish-and-recycling/rubbish-and-recycling-collections-lookup/"
